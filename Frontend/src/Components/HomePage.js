daa5d9fb7e255b1227b287c391c9173c7352560f
Sooryakumar-R <20i342@psgtech.ac.in>
2024-08-20 08:34:50 +0530

    First

---
+++ Frontend/src/Components/HomePage.js
@@ -0,0 +1,22 @@
+import "../Styles/HomePage.css"
+
+function HomePage(){
+    return(
+        <>
+        <div id="header">
+            <h2 id="home"><a href="">Home</a></h2>
+            <h2><a href="">Contact</a></h2>
+            <h2><a href="">Resume</a></h2>
+            <h2><a href="">Github</a></h2>
+            <h2><a href="">Linkedin</a></h2>
+            <h2><a href="">Share</a></h2>
+        </div>
+        <div id="test">
+            <br></br><br></br>
+            <h2>Body</h2>
+        </div>
+        
+        </>
+    );
+}
+export default HomePage;
